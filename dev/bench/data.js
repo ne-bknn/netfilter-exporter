2c9f1d3ae4c6634044f4d2915b0b08ace9cceb06
github-action-benchmark <github@users.noreply.github.com>
2024-07-08 07:17:24 +0000

    add go benchmark (go) benchmark result for f3aacc79cf0ae5dada0b52691171845c5704b5dd

---
+++ dev/bench/data.js
@@ -0,0 +1,80 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720423044962,
+  "repoUrl": "https://github.com/ne-bknn/netfilter-exporter",
+  "entries": {
+    "go benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "36561672+ne-bknn@users.noreply.github.com",
+            "name": "Timofey Bakunin",
+            "username": "ne-bknn"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f3aacc79cf0ae5dada0b52691171845c5704b5dd",
+          "message": "Update test.yml",
+          "timestamp": "2024-07-08T10:17:03+03:00",
+          "tree_id": "fde70dc6f066b84564c0f77f4e727e42884f6dcc",
+          "url": "https://github.com/ne-bknn/netfilter-exporter/commit/f3aacc79cf0ae5dada0b52691171845c5704b5dd"
+        },
+        "date": 1720423044560,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkParseRuleAnnotation",
+            "value": 4888,
+            "unit": "ns/op\t    3320 B/op\t      49 allocs/op",
+            "extra": "241647 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseRuleAnnotation - ns/op",
+            "value": 4888,
+            "unit": "ns/op",
+            "extra": "241647 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseRuleAnnotation - B/op",
+            "value": 3320,
+            "unit": "B/op",
+            "extra": "241647 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseRuleAnnotation - allocs/op",
+            "value": 49,
+            "unit": "allocs/op",
+            "extra": "241647 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseIPTablesXML",
+            "value": 266379,
+            "unit": "ns/op\t   68960 B/op\t    1839 allocs/op",
+            "extra": "4474 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseIPTablesXML - ns/op",
+            "value": 266379,
+            "unit": "ns/op",
+            "extra": "4474 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseIPTablesXML - B/op",
+            "value": 68960,
+            "unit": "B/op",
+            "extra": "4474 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkParseIPTablesXML - allocs/op",
+            "value": 1839,
+            "unit": "allocs/op",
+            "extra": "4474 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
